cc97c1588c12873a2e8c79948cedf5afbbd7d5b6
thirozumi <thirozumi@gmail.com>
2019-07-18 00:08:14 +0900

    added untracked directory

---
+++ 183171-lin/phase-music/assets/scripts/main.js
@@ -0,0 +1,61 @@
+let audioContext = new AudioContext();
+
+function startLoop(audioBuffer, pan = 0, rate = 1) {
+  let sourceNode = audioContext.createBufferSource();
+  let pannerNode = audioContext.createStereoPanner();
+
+  sourceNode.buffer = audioBuffer;
+  sourceNode.loop = true;
+  sourceNode.loopStart = 2.98;
+  sourceNode.loopEnd = 3.80;
+
+  sourceNode.playbackRate.value = rate;
+  pannerNode.pan.value = pan;
+  sourceNode.connect(pannerNode);
+  pannerNode.connect(audioContext.destination);
+  sourceNode.start(0, 2.98);
+}
+
+fetch('assets/sounds/itsgonnarain.mp3')
+  .then(function (response) {
+    return response.arrayBuffer();
+  })
+  .then(function (arrayBuffer) {
+    return audioContext.decodeAudioData(arrayBuffer);
+  })
+  .then(function (audioBuffer) {
+    startLoop(audioBuffer, -1);
+    startLoop(audioBuffer, 1, 1.002);
+  })
+  .catch(function (e) {
+    return console.error(e);
+  });
+
+function setSlide() {
+  const first = document.querySelector('.visual');
+  const before = document.querySelector('.is-show');
+
+  if (before) {
+    before.classList.remove('is-show');
+
+    const next = before.nextElementSibling;
+    if (next) {
+      next.classList.add('is-show');
+    } else {
+      first.classList.add('is-show');
+    }
+  } else {
+    first.classList.add('is-show');
+
+  }
+}
+const slideToggleTarget = document.querySelector('.visuals');
+slideToggleTarget.addEventListener('click', setSlide);
+
+// $('#select').on("change", function (e) {
+//   var value = $(this).val();
+
+//   $('.mode').css({
+//     'mix-blend-mode': value
+//   });
+// });
